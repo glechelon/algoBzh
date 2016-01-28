de7faccff63cd246c1d381e3789f75d0acb37e15
darkguichou <lechelon.guillaume@gmail.com>
2016-01-28 22:18:47 +0100

    ouai modifs ouai ouai

---
+++ vues/error.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang='fr'>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="pages/styleCss.css">
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <title>ERREUR</title>
+
+    <script type="text/javascript" src="../JS/lib.js"></script>
+</head>
+
+<body>
+
+<div class="col-lg-3"></div>
+<div class="col-lg-6">Une erreur s'est produite.</div>
+<div class="col-lg-3"></div>
+</body>
+</html>
+
